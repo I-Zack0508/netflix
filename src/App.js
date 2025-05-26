cb8ad3df0e185a4d7c19ab232aed01a3b40a4a85
isaac <isaacrfeijooficial@gmail.com>
2025-05-26 15:27:32 -0300

    o que foi feito atéa agora 15:27 aula 26/05/25

--- src/App.js
+++ src/App.js
@@ -1,32 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 import './App.css';
 
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === 'originals');
+      let randomChosen = Math.floor(
+        Math.random() * originals[0].items.results.length
+      );
+let chosen = originals[0].items.results[randomChosen];
+let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+setFeaturedData(chosenInfo);
     };
     loadAll();
   }, [])
 
   return (
     <div className='page'>
+
+      {FeaturedData && <FeaturedMovie item={featuredData} />}
+
       <section className='lists'>
         {movieList.map((item, key) => (
-          <MovieRow 
-          key={key} 
-          title={item.title}
-          items={item.items}
+          <MovieRow
+            key={key}
+            title={item.title}
+            items={item.items}
           />
         ))}
       </section>
     </div>
   )
 };
+
+
+// AMO PROGRAMAÇÃO DE CORAÇÃO 😊😊😊😊👍👍😁😁💕💕
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// socorro 🥲🥲🥲🥲
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -5,17 +5,19 @@ import '../components/MovieRow.css';
 
 export default ({ title, items }) => {
     return (
-        <div >
+        <div className="movieRow" >
             <h2>{title}</h2>
             <div className="movieRow-listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-
-                    <img
-                        src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-                        alt={item.original_title || item.name}
-                    />
-
-                ))}
+                <div className="movieRow-list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div Key={key} className="movieRow-item">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                                alt={item.original_title || item.name}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
