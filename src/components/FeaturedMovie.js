8e3148e697092bcb271aaa5f13ca3e47a76de2a1
isaac <isaacrfeijooficial@gmail.com>
2025-06-02 15:58:24 -0300

    listando coisas no banner

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,13 @@ import './FeaturedMovie.css';
 
 export default ({ item }) => {
     if (!item) return null;
+
+    let  firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for (let i in item.genres){
+        genres.push(item.genres[i].name)
+    }
+    
     return (
         <section className="featured"
             style={{
@@ -16,7 +23,17 @@ export default ({ item }) => {
                     <div className="featured-name">{item.original_name}</div>
                     <div className="featured-info">
                         <div className="featured-points">{item.vote_average} pontos</div>
-                        <div className="featured-year">2025</div>
+                        <div className="featured-year">{firstDate.getFullYear()}</div>
+                        <div className="featured-seasons">
+
+                            {item.number_of_seasons} temporada
+                            {item.number_of_seasons !== 1 ? 's' : ''}
+                        </div>
+                        <div className="featured-description">{item.overview}</div>
+                        <div className="featured-buttons"></div>
+                        <div className="featured-genres">
+                            <strong>Gêneros:</strong>{genres.join(', ')}
+                        </div>
                     </div>
                 </div>
             </div>
